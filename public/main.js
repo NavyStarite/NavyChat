773dabd91e77886947ca8593c05f7cd90a1054b7
NavyStarite <61215793+NavyStarite@users.noreply.github.com>
2020-12-14 20:47:49 -0600

    Push actual

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+var socket = io.connect('https://chatting-navy.herokuapp.com/', { 'forceNew': true });
 //Parte del cliente conectamos con localhost
 //escuchamos el evento messages
 // data tendr� el array de mensajes  que env�a el servidor
